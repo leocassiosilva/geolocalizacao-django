7aae34bc050a9446168d438b0169f23af15b195d
leocassiosilva <leocassiosilva1234@gmail.com>
2021-04-11 09:01:23 -0300

    criando a view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import View
+
+from .utils import yelp_seach, get_client_city_data
+
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        items = []
+
+        city = None
+
+        while not city:
+            ret = get_client_city_data()
+            city = ret['city']
+
+        q = request.Get.get('key', None)
+        loc = request.Get.get('loc', None)
+        location = city
+
+        contex = {
+            'city': city,
+            'busca': False
+        }
+
+        if loc:
+            location = loc
+        if q:
+            items = yelp_seach(keyword=q, location=location)
+            contex = {
+                'items': items,
+                'city': location,
+                'busca': True
+            }
+        return render(request, 'index.html', contex)
